39b1bd5fdd0dc1bf6d51e21bdc0440ca5f9d1622
Vinicius Mendite <vinicius@sharbe.com.br>
2021-07-23 11:49:52 -0300

    home responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,47 +11,47 @@ export default function Home() {
     <Box as="main" pb="10">
       <Flex bgImage="/images/background.jpg" bgSize="cover" bgPos="center" w="100%" py="20">
         <Flex position="relative" w="100%" maxW={1240} mx="auto">
-          <Flex flexDir="column">
-            <Heading as="h1" color="gray.50" fontSize="4xl" fontWeight="medium">
+          <Flex flexDir="column" px={['4', '4', '4', '4', '0']}>
+            <Heading as="h1" lineHeight="1.5" color="gray.50" fontSize={['xl', '4xl']} fontWeight="medium">
               5 continentes, <br /> infinitas possibilidades.
             </Heading>
-            <Text color="gray.200" fontSize="xl" mt="5" maxW={524}>
+            <Text color="gray.200" fontSize={['sm', 'xl']} mt="5" maxW={524}>
               Chegou a hora de tirar do papel a viagem que você sempre sonhou.
             </Text>
           </Flex>
-          <Box position="absolute" right="0">
+          <Box position="absolute" right="0" display={['none', 'none', 'none', 'block']}>
             <Image src="/images/airplane.svg" alt="airplane" />
           </Box>
         </Flex>
       </Flex>
 
-      <Box w="100%" maxW={1240} mx="auto" mt="20">
-        <Flex justify="space-between">
-          <Box>
+      <Box w="100%" maxW={1240} mx="auto" mt="20" px={['4', '4', '4', '4', '0']}>
+        <Flex align="center" justify="center" flexWrap="wrap">
+          <Box w={200}>
             <Flex flexDir="column" align="center">
               <Image src="/images/cocktail.svg" alt="cocktail" width={20} height={20} />
               <Text as="span" mt="6" fontSize="2xl" fontWeight="semibold">vida noturna</Text>
             </Flex>
           </Box>
-          <Box>
+          <Box w={200}>
             <Flex flexDir="column" align="center">
               <Image src="/images/surf.svg" alt="surf" width={20} height={20} />
               <Text as="span" mt="6" fontSize="2xl" fontWeight="semibold">praia</Text>
             </Flex>
           </Box>
-          <Box>
+          <Box w={200}>
             <Flex flexDir="column" align="center">
               <Image src="/images/building.svg" alt="building" width={20} height={20} />
               <Text as="span" mt="6" fontSize="2xl" fontWeight="semibold">moderno</Text>
             </Flex>
           </Box>
-          <Box>
+          <Box w={200}>
             <Flex flexDir="column" align="center">
               <Image src="/images/museum.svg" alt="museum" width={20} height={20} />
               <Text as="span" mt="6" fontSize="2xl" fontWeight="semibold">clássico</Text>
             </Flex>
           </Box>
-          <Box>
+          <Box w={200}>
             <Flex flexDir="column" align="center">
               <Image src="/images/earth.svg" alt="earth" width={20} height={20} />
               <Text as="span" mt="6" fontSize="2xl" fontWeight="semibold">e mais...</Text>
@@ -62,7 +62,7 @@ export default function Home() {
         <Box>
           <Flex flexDir="column" mt="20" justify="center" align="center">
             <Box h={0.5} w={90} background="gray.400" />
-            <Heading textAlign="center" mt="12" fontSize="4xl" fontWeight="medium">
+            <Heading textAlign="center" mt="12" fontSize={['xl', '4xl']} fontWeight="medium">
               Vamos nessa? <br />
               Então escolha seu continente
             </Heading>
@@ -79,10 +79,10 @@ export default function Home() {
                   <Flex as="a" flexDir="column" align="center" bgImage="/images/continent-image.jpg" bgSize="cover" bgPosition="center" py="44" _hover={{
                     cursor: 'pointer',
                   }}>
-                    <Text as="strong" fontSize="5xl" fontWeight="bold" color="gray.50">
+                    <Text as="strong" fontSize={['2xl', '5xl']} fontWeight="bold" color="gray.50">
                       Europa
                     </Text>
-                    <Text as="span" color="gray.200" fontSize="2xl" fontWeight="bold">
+                    <Text as="span" color="gray.200" fontSize={['sm', '2xl']} fontWeight="bold">
                       O continente mais antigo.
                     </Text>
                   </Flex>
@@ -93,10 +93,10 @@ export default function Home() {
                   <Flex as="a" flexDir="column" align="center" bgImage="/images/continent-image.jpg" bgSize="cover" bgPosition="center" py="44" _hover={{
                     cursor: 'pointer',
                   }}>
-                    <Text as="strong" fontSize="5xl" fontWeight="bold" color="gray.50">
+                    <Text as="strong" fontSize={['2xl', '5xl']} fontWeight="bold" color="gray.50">
                       Europa
                     </Text>
-                    <Text as="span" color="gray.200" fontSize="2xl" fontWeight="bold">
+                    <Text as="span" color="gray.200" fontSize={['sm', '2xl']} fontWeight="bold">
                       O continente mais antigo.
                     </Text>
                   </Flex>
@@ -107,10 +107,10 @@ export default function Home() {
                   <Flex as="a" flexDir="column" align="center" bgImage="/images/continent-image.jpg" bgSize="cover" bgPosition="center" py="44" _hover={{
                     cursor: 'pointer',
                   }}>
-                    <Text as="strong" fontSize="5xl" fontWeight="bold" color="gray.50">
+                    <Text as="strong" fontSize={['2xl', '5xl']} fontWeight="bold" color="gray.50">
                       Europa
                     </Text>
-                    <Text as="span" color="gray.200" fontSize="2xl" fontWeight="bold">
+                    <Text as="span" color="gray.200" fontSize={['sm', '2xl']} fontWeight="bold">
                       O continente mais antigo.
                     </Text>
                   </Flex>
@@ -121,10 +121,10 @@ export default function Home() {
                   <Flex as="a" flexDir="column" align="center" bgImage="/images/continent-image.jpg" bgSize="cover" bgPosition="center" py="44" _hover={{
                     cursor: 'pointer',
                   }}>
-                    <Text as="strong" fontSize="5xl" fontWeight="bold" color="gray.50">
+                    <Text as="strong" fontSize={['2xl', '5xl']} fontWeight="bold" color="gray.50">
                       Europa
                     </Text>
-                    <Text as="span" color="gray.200" fontSize="2xl" fontWeight="bold">
+                    <Text as="span" color="gray.200" fontSize={['sm', '2xl']} fontWeight="bold">
                       O continente mais antigo.
                     </Text>
                   </Flex>
@@ -135,10 +135,10 @@ export default function Home() {
                   <Flex as="a" flexDir="column" align="center" bgImage="/images/continent-image.jpg" bgSize="cover" bgPosition="center" py="44" _hover={{
                     cursor: 'pointer',
                   }}>
-                    <Text as="strong" fontSize="5xl" fontWeight="bold" color="gray.50">
+                    <Text as="strong" fontSize={['2xl', '5xl']} fontWeight="bold" color="gray.50">
                       Europa
                     </Text>
-                    <Text as="span" color="gray.200" fontSize="2xl" fontWeight="bold">
+                    <Text as="span" color="gray.200" fontSize={['sm', '2xl']} fontWeight="bold">
                       O continente mais antigo.
                     </Text>
                   </Flex>
